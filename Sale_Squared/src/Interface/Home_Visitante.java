8352d23ce5721dd60c0b6feea033edf19c9bb4ba
Ricardo Branco <28.ricardobranco@gmail.com>
2012-12-17 17:49:53 +0000

    Home_categorias_1

--- Sale_Squared/src/Interface/Home_Visitante.java
+++ Sale_Squared/src/Interface/Home_Visitante.java
@@ -1,23 +1,25 @@
 package Interface;
 
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
-import javax.swing.JLayeredPane;
-import javax.swing.JComboBox;
-import java.awt.ScrollPane;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.UIManager;
 
 public class Home_Visitante extends JFrame {
 
@@ -65,17 +67,17 @@ public class Home_Visitante extends JFrame {
 		
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-						.addComponent(panel, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 784, Short.MAX_VALUE)
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_contentPane.createSequentialGroup()
 							.addGap(58)
 							.addComponent(lblNewLabel)
 							.addPreferredGap(ComponentPlacement.RELATED, 272, Short.MAX_VALUE)
 							.addComponent(btnNewButton_1)
 							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(btnNewButton)))
+							.addComponent(btnNewButton))
+						.addComponent(panel, GroupLayout.DEFAULT_SIZE, 784, Short.MAX_VALUE))
 					.addContainerGap())
 		);
 		gl_contentPane.setVerticalGroup(
@@ -86,17 +88,15 @@ public class Home_Visitante extends JFrame {
 						.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
 							.addComponent(btnNewButton)
 							.addComponent(btnNewButton_1)))
-					.addPreferredGap(ComponentPlacement.RELATED, 16, Short.MAX_VALUE)
-					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 226, GroupLayout.PREFERRED_SIZE)
-					.addGap(15))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 230, Short.MAX_VALUE)
+					.addGap(11))
 		);
-		
-		JScrollPane scrollPane = new JScrollPane();
 		
 		textField = new JTextField();
 		textField.setColumns(10);
 		
-		JComboBox comboBox = new JComboBox();
+		JComboBox<String> comboBox = new JComboBox<String>();
 		
 		JButton btnNewButton_2 = new JButton("Pesquisar");
 		btnNewButton_2.addActionListener(new ActionListener() {
@@ -105,19 +105,26 @@ public class Home_Visitante extends JFrame {
 		});
 		
 		JButton btnPesquisaAvanada = new JButton("Pesquisa Avan\u00E7ada");
+		
+		JPanel panel_1 = new JPanel();
+		
+		JScrollPane scrollPane = new JScrollPane();
 		GroupLayout gl_panel = new GroupLayout(panel);
 		gl_panel.setHorizontalGroup(
 			gl_panel.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_panel.createSequentialGroup()
 					.addGap(43)
-					.addComponent(textField, GroupLayout.DEFAULT_SIZE, 276, Short.MAX_VALUE)
-					.addGap(18)
-					.addComponent(comboBox, 0, 210, Short.MAX_VALUE)
-					.addPreferredGap(ComponentPlacement.RELATED)
 					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-						.addComponent(btnNewButton_2, GroupLayout.DEFAULT_SIZE, 199, Short.MAX_VALUE)
-						.addComponent(btnPesquisaAvanada, GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
-					.addGap(18)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addComponent(textField, GroupLayout.DEFAULT_SIZE, 266, Short.MAX_VALUE)
+							.addGap(18)
+							.addComponent(comboBox, 0, 201, Short.MAX_VALUE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+								.addComponent(btnNewButton_2, GroupLayout.DEFAULT_SIZE, 188, Short.MAX_VALUE)
+								.addComponent(btnPesquisaAvanada, GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)))
+						.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 685, Short.MAX_VALUE))
+					.addGap(42)
 					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE))
 		);
 		gl_panel.setVerticalGroup(
@@ -130,9 +137,37 @@ public class Home_Visitante extends JFrame {
 						.addComponent(btnNewButton_2))
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(btnPesquisaAvanada)
-					.addContainerGap(157, Short.MAX_VALUE))
-				.addComponent(scrollPane, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 234, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 108, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(45, Short.MAX_VALUE))
+				.addComponent(scrollPane, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 230, Short.MAX_VALUE)
 		);
+		
+		final JLabel lblNewLabel_1 = new JLabel("Moda");
+		lblNewLabel_1.setForeground(UIManager.getColor("Button.light"));
+		lblNewLabel_1.setFont(new Font(lblNewLabel_1.getFont().getName(), Font.BOLD, lblNewLabel_1.getFont().getSize()+2));
+		lblNewLabel_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				//lblNewLabel_1.setText("EXITO");
+				
+			}
+		});
+		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
+		gl_panel_1.setHorizontalGroup(
+			gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblNewLabel_1)
+					.addContainerGap(645, Short.MAX_VALUE))
+		);
+		gl_panel_1.setVerticalGroup(
+			gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addComponent(lblNewLabel_1)
+					.addContainerGap(92, Short.MAX_VALUE))
+		);
+		panel_1.setLayout(gl_panel_1);
 		panel.setLayout(gl_panel);
 		contentPane.setLayout(gl_contentPane);
 	}
